fb96034373e885b8eed26bfa32a33e2bd2801d76
dalvarez <david.alvarez@usm.cl>
2022-10-21 18:35:41 -0300

    Separación API's Insumos y Productos

--- API_Insumos/API/Scripts/TEST-000001-Crear tabla-bd.sql
+++ API_Insumos/API/Scripts/TEST-000001-Crear tabla-bd.sql
@@ -17,22 +17,3 @@ PRIMARY KEY (ID_Insumo)
 execute sp_addextendedproperty @DESCRIPCION, 
    'Tabla que alamcena los Insumos',
    'user', @CurrentUser, @TABLE, 'Insumos'
-
-CREATE TABLE Productos(
-	CodigoProducto varchar(100) NOT NULL,
-	Nombre varchar(100) NOT NULL,
-   Tipo varchar(20) NOT NULL
-)
-
-ALTER TABLE Productos
-ADD CONSTRAINT PK_Productos
-PRIMARY KEY (CodigoProducto)
-
-ALTER TABLE Productos
-ADD Disponibilidad INT NOT NULL 
-CONSTRAINT D_Productos_Disponible
-DEFAULT (1)
-
-execute sp_addextendedproperty @DESCRIPCION, 
-   'Tabla que alamcena los tipos de Productos',
-   'user', @CurrentUser, @TABLE, 'Productos'
---
+++ API_Productos/API/Scripts/TEST-000002-Valores parametricos-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Productos(CodigoProducto, Nombre, Tipo, Disponibilidad)
+VALUES('VP_01','Viejito Pascuero','Manualidad'),
+      ('PA_01','Conejo de Pascua','Manualidad',0)
+GO
