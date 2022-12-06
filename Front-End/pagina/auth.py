598c970fc1110f8e08b88f1e8d55b4c8927adc53
Elias Martinez Rodriguez <elias.maritnezr@usm.cl>
2022-12-06 03:38:19 -0300

    Creados registros separados para Staff y Clientes

--- Front-End/pagina/auth.py
+++ Front-End/pagina/auth.py
@@ -6,6 +6,10 @@ auth = Blueprint('auth', __name__)
 def login():
     return render_template("sesion.html")
 
-@auth.route('/sign-up', methods=['GET','POST'])
-def registrar():
-    return render_template("registrar.html")
+@auth.route('/sign-up-client', methods=['GET','POST'])
+def registrar_cliente():
+    return render_template("registrar cliente.html")
+
+@auth.route('/sign-up-staff', methods=['GET','POST'])
+def registrar_staff():
+    return render_template("registrar staff.html")
