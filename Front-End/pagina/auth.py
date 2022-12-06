357e440b2d865219588debe31726b95e288fa5a2
Elias Martinez Rodriguez <elias.maritnezr@usm.cl>
2022-12-06 03:25:19 -0300

    Creada pagina para crear cuenta

--- Front-End/pagina/auth.py
+++ Front-End/pagina/auth.py
@@ -5,3 +5,7 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET','POST'])
 def login():
     return render_template("sesion.html")
+
+@auth.route('/sign-up', methods=['GET','POST'])
+def registrar():
+    return render_template("registrar.html")
